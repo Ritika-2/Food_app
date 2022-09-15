93cafa5ece432f7352f0bb550db2a23c36e0e7ec
Kumari Diksha Singh <kumaridiksha.singh@clarivate.com>
2022-09-15 22:34:16 +0530

    User and Item CRUD operation updated

--- src/main/java/com/org/foodapp/controller/FoodOrderController.java
+++ src/main/java/com/org/foodapp/controller/FoodOrderController.java
@@ -3,6 +3,7 @@ package com.org.foodapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.org.foodapp.dto.FoodOrder;
+import com.org.foodapp.responseStructure.ResponseStructure;
 import com.org.foodapp.services.FoodOrderService;
 
 @RestController
@@ -20,28 +22,33 @@ public class FoodOrderController {
 	@Autowired
 	FoodOrderService foodOrderService ;
 	
-	@PostMapping("/foodOrder")
-	public FoodOrder saveFoodOrder(@RequestBody FoodOrder foodOrder) {
-		return foodOrderService.saveFoodOrder(foodOrder);
+	@PostMapping("/foodorder")
+	public ResponseEntity<ResponseStructure<FoodOrder>> saveFoodOrder(@RequestBody FoodOrder foodOrder){
+		return foodOrderService.saveFoodOrder(foodOrder, 1);
 	}
 	
-	@PutMapping("/updatefoodorder")
-	public FoodOrder updateFoodOrder(@RequestBody FoodOrder foodOrder) {
-		return foodOrderService.updateFoodOrder(foodOrder);
-	}
-	
-	@GetMapping("/foodorders")
-	public List<FoodOrder> getAll(){
-		return foodOrderService.getAllFoodOrders();
-	}
-	
-	@GetMapping("/foodorders/{id}")
-	public FoodOrder getfoodOrderById(@PathVariable int id) {
-		return foodOrderService.getfoodOrderById(id);
-	}
-	
-	@DeleteMapping("/foodorders/{id}")
-	public String delete(@PathVariable int id) {
-		return foodOrderService.delete(id);
-	}
+//	@PostMapping("/foodOrder")
+//	public FoodOrder saveFoodOrder(@RequestBody FoodOrder foodOrder) {
+//		return foodOrderService.saveFoodOrder(foodOrder);
+//	}
+//	
+//	@PutMapping("/updatefoodorder")
+//	public FoodOrder updateFoodOrder(@RequestBody FoodOrder foodOrder) {
+//		return foodOrderService.updateFoodOrder(foodOrder);
+//	}
+//	
+//	@GetMapping("/foodorders")
+//	public List<FoodOrder> getAll(){
+//		return foodOrderService.getAllFoodOrders();
+//	}
+//	
+//	@GetMapping("/foodorders/{id}")
+//	public FoodOrder getfoodOrderById(@PathVariable int id) {
+//		return foodOrderService.getfoodOrderById(id);
+//	}
+//	
+//	@DeleteMapping("/foodorders/{id}")
+//	public String delete(@PathVariable int id) {
+//		return foodOrderService.delete(id);
+//	}
 }
